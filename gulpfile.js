de3b626bc525b106f9c65c8db800a3f71c662231
Marius Rizac <marius.rizac@spryker.com>
2016-05-27 22:56:53 +0200

    updated gulpfile and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -4,34 +4,47 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var plumber = require('gulp-plumber');
 
+var sources = {
+    js: {
+        external: [
+            'bower/jquery/dist/jquery.js',
+            'bower/angular/angular.js',
+            'bower/angular-route/angular-route.js',
+            'bower/angular-resource/angular-resource.js',
+            'bower/angular-animate/angular-animate.js',
+            'bower/sweetalert/dist/sweetalert.min.js'
+        ],
+        internal: [
+            'src/js/app.module.js',
+            'src/js/app.config.js',
+            'src/js/core/core.module.js',
+            'src/js/core/checkmark/checkmark.filter.js',
+            'src/js/core/phone/phone.module.js',
+            'src/js/core/phone/phone.service.js',
+            'src/js/phone-list/phone-list.module.js',
+            'src/js/phone-list/phone-list.component.js',
+            'src/js/phone-detail/phone-detail.module.js',
+            'src/js/phone-detail/phone-detail.component.js',
+            'src/js/animation.js'
+        ]
+    },
+    css: [
+        'bower/bootstrap/dist/css/bootstrap.css',
+        'bower/sweetalert/dist/sweetalert.css',
+        'src/css/animate.css',
+        'src/css/index.css'
+    ]
+};
+
 gulp.task('js-external', function(){
-    gulp.src([
-        'bower/jquery/dist/jquery.js',
-        'bower/angular/angular.js',
-        'bower/angular-route/angular-route.js',
-        'bower/angular-resource/angular-resource.js',
-        'bower/angular-animate/angular-animate.js',
-        'bower/sweetalert/dist/sweetalert.min.js'
-    ])
+    gulp.src(sources.js.external)
         .pipe(concat('external.js'))
         .pipe(gulp.dest('app/dist/js'))
     ;
 });
 
 gulp.task('js', function(){
-    gulp.src([
-        'app/src/js/app.module.js',
-        'app/src/js/app.config.js',
-        'app/src/js/core/core.module.js',
-        'app/src/js/core/checkmark/checkmark.filter.js',
-        'app/src/js/core/phone/phone.module.js',
-        'app/src/js/core/phone/phone.service.js',
-        'app/src/js/phone-list/phone-list.module.js',
-        'app/src/js/phone-list/phone-list.component.js',
-        'app/src/js/phone-detail/phone-detail.module.js',
-        'app/src/js/phone-detail/phone-detail.component.js',
-        'app/src/js/animation.js'
-    ])
+    gulp.src(sources.js.internal)
         .pipe(plumber())
         .pipe(concat('bundle.js'))
         .pipe(gulp.dest('app/dist/js'))
@@ -39,20 +52,15 @@ gulp.task('js', function(){
 });
 
 gulp.task('css', function(){
-    gulp.src([
-        'bower/bootstrap/dist/css/bootstrap.css',
-        'bower/sweetalert/dist/sweetalert.css',
-        'app/src/css/animate.css',
-        'app/src/css/index.css'
-    ])
+    gulp.src(sources.css)
         .pipe(concat('bundle.css'))
         .pipe(gulp.dest('app/dist/css'))
     ;
 });
 
 gulp.task('watch', ['js', 'css'], function(){
-    gulp.watch(['app/src/js/**/**.js'], ['js']);
-    gulp.watch(['app/src/css/**/**.css'], ['css']);
+    gulp.watch(['src/js/**/**.js'], ['js']);
+    gulp.watch(['src/css/**/**.css'], ['css']);
 });
 
 gulp.task('default', ['js', 'css', 'js-external']);
